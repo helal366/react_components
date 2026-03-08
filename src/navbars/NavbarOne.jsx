e48edc16d59b0a53b2f36b7fcf8076d9a59e64f3
Helal366 <helal56876@gmail.com>
2026-03-08 09:29:30 +0600

    novbar one responsive menu polished

--- src/navbars/NavbarOne.jsx
+++ src/navbars/NavbarOne.jsx
@@ -1,19 +1,51 @@
 import { Menu, X } from 'lucide-react';
 import React, { useState } from 'react';
 
+const navData = [
+  {
+    id: 1,
+    path: "/",
+    name: "Home"
+  },
+  {
+    id: 2,
+    path: "/about",
+    name: "About"
+  },
+  {
+    id: 3,
+    path: "/services",
+    name: "Services"
+  },
+  {
+    id: 4,
+    path: "/blog",
+    name: "Blog"
+  },
+  {
+    id: 5,
+    path: "/contact",
+    name: "Contact"
+  }
+];
 const NavbarOne = () => {
     const [open, setOpen] = useState(false);
-    const links = (
-        <>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-        </>
-    )
+    // const links = (
+    //     <>
+    //         <li>Home</li>
+    //         <li>About</li>
+    //         <li>Contact</li>
+    //     </>
+    // )
+    const links= navData.map(singleLink=> <li key={singleLink.id} className='singleNavLink'>
+        <a href={singleLink.path}>
+           {singleLink.name}
+        </a>
+    </li>)
     return (
-        <nav className="navbar bg-[#b2e32a80] shadow-sm z-10 flex justify-between padding">
+        <nav className="navbar bg-[#b2e32a80] shadow-sm z-10 flex justify-between padding py-3">
             <div className='flex gap-2 items-center relative'>
-                <ul className={`w-40 rounded absolute duration-500 bg-[#6b8383] lg:hidden p-6 ${open ? "flex flex-col gap-3 top-11" : "-top-60"}`}>
+                <ul className={`navLinks ${open ? "flex flex-col gap-2 top-11" : "-top-80"}`}>
                     {links}
                 </ul>
                 <button onClick={() => setOpen(!open)} className='lg:hidden cursor-pointer'>
